d95626a388c540b981ec300e86b06bf893866a56
pabloleal <118738402+Pabloricardo17@users.noreply.github.com>
2025-10-26 23:59:52 +0000

    Adicionado prp nova no sistema

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,7 @@
-import React from 'react';
-import {Text} from 'react-native';
-
 function Home(){
     return (
        
-         <Text>Projeto Git! Em desenvolvimento com varios commits e melhorias constantes.</Text>
+         <h1>Projeto Git! Em desenvolvimento com varios recursos</h1>
     
     );
 }
