9e24aa20ccd0d99d9d741ca7091ed95fa91a9e01
pabloleal <118738402+Pabloricardo17@users.noreply.github.com>
2025-11-02 20:59:37 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,16 @@
-function Home(){
-    return (
-       
-         <h1>Projeto Git! Em desenvolvimento com varios recursos</h1>
-    
-    );
+function Home() {
+  return <h1>Projeto Git! Em desenvolvimento com varios recursos</h1>;
+}
+
+function teste() {
+  console.log("Teste");
+}
+function teste2() {
+  console.log("Teste2");
+}
+
+function teste4() {
+  console.log("teste3");
 }
 
 export default Home;
